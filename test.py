a318336bd068021449581d908016a041077672fd
Valentino-Moreno <v.moreno@alumno.etec.um.edu.ar>
2022-08-23 12:48:17 -0300

    terminado

--- test.py
+++ test.py
@@ -155,6 +155,61 @@ pdf.ln(0)
 pdf.cell(122)
 pdf.cell(140, 0, 'FIRMA DEL COLABORADOR', 0)
 
+#imprime texto
+pdf.ln(15)
+pdf.cell(8)
+pdf.cell(0, 0, 'OBSERVACIONES:', 0)
+#define tipo de letra
+pdf.set_font('Arial', '',8)
+#imprime texto
+pdf.ln(5)
+pdf.cell(8)
+pdf.cell(0, 0, 'variable variable variable variable', 0)
+
+#hace los rectangulos
+pdf.set_fill_color(200,200,200)
+pdf.rect(x = 10, y = 240, w = 124, h = 8, style = 'DF')
+pdf.rect(x = 134, y = 240, w = 66, h = 8, style = 'DF')
+#define el tipo de letra, tamaño y color
+pdf.set_font('Arial', 'B',12)
+pdf.set_fill_color(110,110,110)
+#imprime texto
+pdf.ln(50)
+pdf.cell(50)
+pdf.cell(0, 0, 'APROBADO', 0)
+#imprime texto
+pdf.ln(0)
+pdf.cell(150)
+pdf.cell(140, 0, 'VERIFICADO', 0)
+#creamos rectangulos
+pdf.rect(x = 10, y = 248, w = 62, h = 25, style = '')
+pdf.rect(x = 72, y = 248, w = 62, h = 25, style = '')
+pdf.rect(x = 134, y = 248, w = 66, h = 25, style = '')
+#creamos rectangulos
+pdf.rect(x = 10, y = 248, w = 62, h = 25, style = '')
+pdf.rect(x = 72, y = 248, w = 62, h = 25, style = '')
+pdf.rect(x = 134, y = 248, w = 66, h = 25, style = '')
+#creamos rectangulos
+pdf.rect(x = 10, y = 273, w = 62, h = 6, style = '')
+pdf.rect(x = 72, y = 273, w = 62, h = 6, style = '')
+pdf.rect(x = 134, y = 273, w = 66, h = 6, style = '')
+#imprime texto
+pdf.ln(32)
+pdf.cell(35)
+pdf.cell(140, 0, 'JEFE', 0)
+#imprime texto
+pdf.ln(0)
+pdf.cell(90)
+pdf.cell(140, 0, 'GERENTE', 0)
+#imprime texto
+pdf.ln(0)
+pdf.cell(140)
+pdf.cell(130, 0, 'GERENCIA DE R.R.H.H.', 0)
+
+
+
+
+
 
 
 
